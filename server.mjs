9a57eb4244e404b535ce28e5e70c9f5ebab5da36
JECisternas <jcr_mw@yahoo.com>
2022-10-15 08:43:08 -0600

    hola

--- server.mjs
+++ server.mjs
@@ -3,9 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    console.log('Response from Jaime: ' + req.ip);
-    res.send('Response from Jaime: ' + req.ip) 
-
+    console.log('Response from Jaime Cisternas: ' + req.ip);
+    res.send('Response from Jaime Cisternas: ' + req.ip)
 })
 
 app.listen(port, () => {
